4e1404f8eee7e9cef9909b7cf33f89223b122d5d
pedrosaints <pedro.santos@fixpay.com>
2026-01-22 17:16:30 -0300

    feat: INIT Shutdown

--- service.go
+++ service.go
@@ -3,28 +3,74 @@ package goutils
 import (
 	"fmt"
 	"github.com/kardianos/service"
+	"sync"
 )
 
-type program struct{}
+type program struct {
+	// Use a wait group to track active goroutines
+	wg      sync.WaitGroup
+	countWg int
+}
 
-func (p program) Start(s service.Service) error {
+var programInstance *program
+var ServiceInstance service.Service
+
+func (p *program) Start(s service.Service) error {
 	fmt.Println(s.String() + " Rodando!")
 	return nil
 }
 
-func (p program) Stop(s service.Service) error {
+func (p *program) Stop(s service.Service) error {
+	fmt.Println(fmt.Sprintf("%s Parando com %d threads ainda rodando...", s.String(), p.countWg))
+	p.DoneWaitGroup()
+
+	p.wg.Wait()
 	fmt.Println(s.String() + " Parado!")
 	return nil
 }
 
+func (p *program) AddWaitGroup() {
+	p.wg.Add(1)
+	p.countWg++
+}
+
+func (p *program) DoneWaitGroup() {
+	fmt.Println("Finishing Thread")
+	p.wg.Done()
+	p.countWg--
+}
+
+func InitService() {
+	programInstance = &program{
+		wg: sync.WaitGroup{},
+	}
+	programInstance.AddWaitGroup()
+}
+
 func ExecuteService(serviceConfig *service.Config) {
-	prg := &program{}
-	s, err := service.New(prg, serviceConfig)
+	if programInstance == nil {
+		InitService()
+	}
+
+	var err error
+	ServiceInstance, err = service.New(programInstance, serviceConfig)
 	if err != nil {
 		fmt.Println("Não pode criar o serviço: " + err.Error())
 	}
-	err = s.Run()
+
+	err = ServiceInstance.Run()
 	if err != nil {
 		fmt.Println("Não é possível iniciar o serviço: " + err.Error())
 	}
+}
+
+func AddThreadForShutdown() {
+	if programInstance == nil {
+		InitService()
+	}
+	programInstance.AddWaitGroup()
+}
+
+func DoneThreadForShutdown() {
+	programInstance.DoneWaitGroup()
 }
